78eabffcc1fe0743a730557840e7ca9ff88f7500
FacundoArguet <facu08nacho13@gmail.com>
2026-07-13 20:07:08 +0200

    septimo intetento

---
+++ mainesca.java
@@ -0,0 +1,18 @@
+package programacion;
+import java.util.Scanner;
+
+public class mainesca {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la red (Ej: 192.168.1.): ");
+        String red = sc.nextLine();
+
+        System.out.println("Analizando...");
+        escaneo escaneo = new escaneo();
+        escaneo.escanear(red);
+
+        sc.close();
+    }
+}
