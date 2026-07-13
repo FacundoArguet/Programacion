84076435c58b176466dba4bff7dcab2209a080bb
FacundoArguet <facu08nacho13@gmail.com>
2026-07-13 20:04:36 +0200

    sexto intetento

--- escaneo.java
+++ escaneo.java
@@ -1,4 +1,4 @@
-package programacion;
+package programacion.Redes;
 
 public class escaneo {
 
--- main.java
+++ main.java
@@ -1,5 +1,4 @@
-package programacion;
-
+package programacion.Redes;
 import java.util.Scanner;
 
 public class main {
@@ -7,38 +6,12 @@ public class main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la red (Ej: 192.168.1.): ");
+        String red = sc.nextLine();
 
-        System.out.print("Ingrese el nombre: ");
-        String nombre = sc.nextLine();
-
-        System.out.print("Ingrese la salud: ");
-        int salud = sc.nextInt();
-
-        if (salud <= 0) {
-            System.out.println("Estas muerto, LOL");
-            sc.close();
-            return;
-        }
-
-        System.out.print("Ingrese el nivel: ");
-        int nv = sc.nextInt();
-
-        if (nv < 0) {
-            System.out.println("No puedes ser de nivel tan bajo, burro");
-            sc.close();
-            return;
-        }
-
-        while (nv > 1) {
-            System.out.println("Demasiado nivel, vuelva a intentarlo.");
-            nv = sc.nextInt();
-        }
-
-        Personaje personaje = new Personaje(nombre, salud, nv);
-
-        System.out.println("Nombre: " + personaje.getnombre());
-        System.out.println("Salud: " + personaje.getsalud());
-        System.out.println("Tu nivel es " + personaje.getnv());
+        System.out.println("Analizando...");
+        escaneo escaneo = new escaneo();
+        escaneo.escanear(red);
 
         sc.close();
     }
